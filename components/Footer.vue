fc401b0ef35921c31ccca88010e299e1f42039cc
sixstorm <sixstorm@gmail.com>
2021-11-26 18:43:22 +0000

    bulma

---
+++ components/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+    <footer>
+        <p>teamtuck 2021</p>
+    </footer>
+</template>
---
+++ components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+    <header>
+        <nuxt-link to="/"></nuxt-link>
+        <nuxt-link to="/blog">Blog</nuxt-link>
+        <nuxt-link to="/tech">Tech</nuxt-link>
+        <nuxt-link to="/movies">Movies</nuxt-link>
+        <nuxt-link to="/books">Books</nuxt-link>
+    </header>        
+</template>
+
