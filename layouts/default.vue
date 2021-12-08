85a699d62edc9145db3316b5b1dfdda03798a9e1
sixstorm <sixstorm@gmail.com>
2021-12-08 13:16:14 -0600

    It's broke

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,12 +7,4 @@
 </template>
 
 <script>
-import Header from "../components/Header.vue";
-import Footer from "../components/Footer.vue";
-export default {
-  components: {
-    Header,
-    Footer
-  }
-}
 </script>
