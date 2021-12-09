19b93b94b55442168d088f5b6655ab3ab2ef895e
sixstorm <sixstorm@gmail.com>
2021-12-09 14:08:14 -0600

    Movies Comp

---
+++ components/listMovies.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div v-for="movie in movies" :key="movie.title">
+            <p>{{ movie.title }}</p>
+            <p>{{ movie.link }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        async asyncData ({ $content }) {
+            const movies = await $content('movies/movies').fetch()
+            return { movies }
+        }
+    }
+</script>
--- pages/test/index.vue
+++ pages/test/index.vue
@@ -1,43 +1,11 @@
 <template>
     <div>
-        <h1 class="is-centered">MOVIES!</h1>
-        <table class="table is-bordered is-hoverable is-fullwidth">
-            <thead>
-                <tr>
-                    <th style="text-align:left">Title</th>
-                    <th style="text-align:left">Link</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="(item, key) of movies" :key="key">
-                    <td>{{ item.title }}</td>
-                    <td><a href="#">{{ item.link }}</a></td>
-                </tr>
-            </tbody>
-        </table>
-        <!-- <ul v-for="(item, key) of movies" :key="key">{{ item.title }}</ul> -->
+    <listMovies />
     </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            movies: [
-                {
-                    title: "Home Alone",
-                    link: "https://www.imdb.com/title/tt0099785/"
-                },
-                {
-                    title: "Harry Potter and the Sorcerer's Stone",
-                    link: "https://www.imdb.com/title/tt0241527/"
-                },
-                {
-                    title: "Cruella",
-                    link: "https://www.imdb.com/title/tt3228774/"
-                }
-            ],
-        }
-    }
+
 }
 </script>
