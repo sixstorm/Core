121e4394cddf03d1e3f023ca6bceabe94dc2d54c
sixstorm <sixstorm@gmail.com>
2021-12-11 15:04:11 -0600

    Video base layout

---
+++ components/showVideos.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="columns">
+        <div class="column is-half has-text-centered" v-for="video in videos" :key="video.title">
+            <p>{{ video.title }}</p>
+            <figure class="image is-16by9">
+                <iframe class="has-ratio" width="560" height="315" v-bind:src="video.link" frameborder="0"></iframe>
+            </figure>
+        </div>
+    </div>
+</template>
+
+<script>
+    import data from "~/content/videos/videos.json";
+    
+    export default {
+        data() {
+            return {
+                videos: data,
+            }
+        }
+    }
+</script>
---
+++ pages/videos/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <section>    
+        <div>
+            <h1 class="">Videos I Enjoy</h1>
+            <showVideos />
+        </div>
+        <div>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/7NOSDKb0HlU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+    </section>    
+</template>
+
+<script>
+export default {
+
+
+}
+</script>
