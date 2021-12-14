9ecb679de7e1a6ac0fcb45c0bb47071d216bdb15
sixstorm <sixstorm@gmail.com>
2021-12-14 10:59:15 -0600

    Adding music page

--- components/Header.vue
+++ components/Header.vue
@@ -1,14 +1,15 @@
 <template>
-    <nav class="level navbar is-spaced">
-        <div class="level-left has-text-centered">
-            <a class="level-item navbar-item" href="/">Core</a>
-        </div>
-        <div class="level-right has-text-centered">
-                <a class="level-item navbar-item" href="/tech">Tech</a>
-                <a class="level-item navbar-item" href="/blog">Blog</a>
-                <a class="level-item navbar-item" href="/movies">Movies</a>
-                <a class="level-item navbar-item" href="/books">Books</a>
-        </div>
-    </nav>
+  <nav class="level navbar is-spaced">
+    <div class="level-left has-text-centered">
+      <a class="level-item navbar-item" href="/">Core</a>
+    </div>
+    <div class="level-right has-text-centered">
+      <a class="level-item navbar-item" href="/tech">Tech</a>
+      <a class="level-item navbar-item" href="/blog">Blog</a>
+      <a class="level-item navbar-item" href="/movies">Movies</a>
+      <a class="level-item navbar-item" href="/books">Books</a>
+      <a class="level-item navbar-item" href="/music">Music</a>
+    </div>
+  </nav>
 </template>
 
---
+++ pages/aboutme/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>About Me</h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
