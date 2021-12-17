48305883589b0beed977ce9680837162cb6fce3e
sixstorm <sixstorm@gmail.com>
2021-12-16 20:50:59 -0600

    Updated movie list

--- components/showArticle.vue
+++ components/showArticle.vue
@@ -1,28 +1,26 @@
 <template>
-    <div>
-        <section>
-            <div class="container">
-                <article>
-                    <div class="mb-6">
-                        <h1 class="has-text-centered-mobile">{{ article.title }}</h1>
-                        <h5 class="has-text-centered-mobile">{{ article.description }}</h5>
-                    </div>
-                    <div class="nuxt-content has-text-centered-mobile">
-                        <Nuxt-Content class="is-size-5-mobile" :document="article" />
-                    </div>
-                </article>
-            </div>
-        </section>
-    </div>
+  <div>
+    <section>
+      <div class="container">
+        <article>
+          <div class="mb-6">
+            <h1 class="has-text-centered-mobile">{{ article.title }}</h1>
+            <h5 class="has-text-centered-mobile">{{ article.description }}</h5>
+          </div>
+          <div class="nuxt-content has-text-centered-mobile">
+            <Nuxt-Content class="is-size-5-mobile" :document="article" />
+          </div>
+        </article>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
 export default {
-    props: ['article']
+  props: ['article'],
 }
 </script>
 
 <style>
-
-
 </style>
